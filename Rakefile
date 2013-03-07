34c986c8f5910be43cda0dac123eecba310917f3
santochan <last1475@126.com>
2013-03-07 14:56:41 +0800

    add a template generator to do layout

---
+++ Rakefile
@@ -0,0 +1,14 @@
+require "./lib/GenHtml.rb"
+
+desc "Generate the demo"
+task :demo  do
+  layout_file = File.dirname(__FILE__)+"/_layout/default.html"
+  Dir.glob(File.dirname(__FILE__)+"/_pages/*.html") do |page_file|
+    output_file = File.dirname(__FILE__)+"/#{File.basename(page_file)}"
+    File.open(page_file,"r",encoding:"UTF-8") do |file|
+      GenHtml.do_layout(layout_file,output_file,file.read)
+    end
+  end
+end
+
+
---
+++ lib/GenHtml.rb
@@ -0,0 +1,22 @@
+require "liquid"
+
+class GenHtml
+  
+
+
+  # Generate file
+  def self.do_layout(layout_file,output_file,content)
+
+    File.open(layout_file, "r",encoding: 'UTF-8') do |layout_file|
+      layout = layout_file.read
+      result = Liquid::Template.parse(layout).render "content" => content
+      File.open(output_file,  "w",encoding: 'UTF-8') do |output_file|
+        output_file.write result
+      end
+    end
+  end
+
+end
+
+
+
